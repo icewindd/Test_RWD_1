128dcbf97c9fa5f1c402c2b984309b66c33dd08c
icewindd <achiang1225@gmail.com>
2017-03-21 22:34:02 +0800

    updata add index effect & cart fix_1

--- js/all.js
+++ js/all.js
@@ -6,5 +6,29 @@ $(document).ready(function() {
   });
   $('.js-like').click(function(event) {
   	event.preventDefault();
-  });
+  });//end on
+
+  $('.scrollTop').click(function(event) {
+  	event.preventDefault();
+  	var target = $(this).attr('href');
+	var targetPos = $(target).offset().top;
+	$('body').animate({scrollTop:targetPos-50},1000);
+	$('scrollTop').removeClass('active');
+	$(this).addClass('active');
+  });//end click
+  $(window).scroll(function(){
+  	var scrollPos = $(window).scrollTop();
+  	if(scrollPos>=100){
+  		$('.top').addClass('active');
+  	}else{
+  		$('.top').removeClass('active');
+  	}
+  });//end scroll
+
+    $('.top').click(function(event){
+  		event.preventDefault();
+  		$('body').animate({scrollTop:0},2000);
+
+  	});//end top click
+
 });
